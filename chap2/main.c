7dadf1fb746ab06b7e8f37fb3abc8d5925b1dbd2
PedroLopes Verardo Ramos <pramos@localhost.localdomain>
2025-05-25 20:44:23 -0300

    start of chapter two

--- chap2/main.c
+++ chap2/main.c
@@ -13,7 +13,26 @@
  *  that are invented by the university of berkely.*/
 
 #include<stdio.h>
-#include<winsock2.h>
+#include<time.h>
+#include<string.h>
+
+
 int main(void){
-  
+  time_t *timer;
+  time(timer);
+
+  printf("local time is: %s\n", ctime(timer));
+
+  printf("Configure local address...\n");
+  struct addrinfo hints;
+
+  menset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  struct addrinfo *bind_address;
+  getaddrinfo(0, "8080", &hints, &bind_address);
+
+  return 0;
 }
